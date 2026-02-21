59c7c6f37fdbe6c1223f73d09ecaff9b9277acd9
3lviend <henryss_10@yahoo.com>
2026-02-21 14:18:35 +0700

    Update styling, and added more color themes

--- src/components/ProfileSection/ProfileSection.tsx
+++ src/components/ProfileSection/ProfileSection.tsx
@@ -5,17 +5,17 @@ interface ProfileSectionProps {
   name: string;
   backgroundImage: string;
   profileImage: string;
-  description: string;
+  description: React.ReactNode;
 }
 
-const ProfileSection: React.FC<ProfileSectionProps> = ({name, backgroundImage, profileImage, description}) => {
+const ProfileSection: React.FC<ProfileSectionProps> = ({ name, backgroundImage, profileImage, description }) => {
   return (<div className={styles.profileSection}>
-    <img src={backgroundImage} alt={name} className={styles.backgroundImage}/>
+    <img src={backgroundImage} alt={name} className={styles.backgroundImage} />
     <div className={styles.profileContainer}>
-      <img src={profileImage} alt={name} className={styles.profileImage}/>
+      <img src={profileImage} alt={name} className={styles.profileImage} />
     </div>
     <h1 className={styles.profileName}>{name}</h1>
-    <p>{description}</p>
+    <p className={styles.profileDescription}>{description}</p>
   </div>);
 };
 
